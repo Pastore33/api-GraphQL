81969f6a91cb26474f015006efd4b27838cb7c4c
Leandro Francisco <leandro.francisco@enube.me>
2023-09-28 23:19:02 -0300

    Project started

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from "apollo-server"
+import "reflect-metadata";
+
+
+async function bootstrap() {
+  const server = new ApolloServer({
+    
+  })
+
+  const { url } = await server.listen()
+
+  console.log(`HTTP server running on ${url}`)
+}
+
+bootstrap()
